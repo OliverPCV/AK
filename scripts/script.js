a6404a570a42165579ea256a0898d2694f6ba593
OliverPCV <oliver.cavallo@educanet.cz>
2020-10-31 00:26:00 +0100

    :)

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,45 @@
 window.addEventListener('load', () => {
-  const mainimage = document.getElementsByClassName("main-image").item(0);
-  const nav = document.getElementById("header");
-
-  function checkScroll() {
-    const isPastImage = window.scrollY > mainimage.clientHeight;
-    if (isPastImage) {
-      nav.classList.add('scrolled');
-      console.log("ahoj");
-    }
-
-    else nav.classList.remove('scrolled');
-  }
-  checkScroll();
   window.addEventListener('scroll', checkScroll);
-
   document.getElementById('navTrigger').addEventListener('click', toggleMenu);
+  document.getElementById('navTrigger').addEventListener('click', menuAnimation);
+
 });
 
+function checkScroll() {
+  const mainimage = document.getElementById("main-image").clientHeight;
+  const nav = document.getElementById("header");
+  var scroll = window.scrollY;
+  
+  if(scroll >= mainimage) nav.classList.toggle('scrolled');
+  else nav.classList.remove('scrolled');
+  
+}
+
+
 function toggleMenu() {
+ 
   const x = document.getElementById("myNav");
   x.classList.toggle("responsive");
   const y = document.getElementById("nav-socials");
   y.classList.toggle("responsive");
 }
+
+const menuAnimation = () => {
+  const x = document.getElementById("navTrigger");
+  x.classList.toggle('active');
+  
+  
+}
+
+/*const navSlide = () => {
+  const burger = document.querySelector('.navTrigger');
+  const nav = document.querySelector('.nav-menu');
+  burger.addEventListener('click', () => {
+    nav.classList.toggle('nav-active');
+  });
+}
+navSlide();
+*/
 
 /*$(document).ready(function () {
   var $mainimage = $(".main-image");
