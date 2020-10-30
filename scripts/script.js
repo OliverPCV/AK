b4a0ae2807313cfc70ae5cad4b17fbd3c4233565
unknown <oliver.cavallo@educanet.cz>
2020-10-30 17:02:20 +0100

    hotovo

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,27 @@
-$(document).ready(function () {
+window.addEventListener('load', () => {
+  const mainimage = document.getElementsByClassName("main-image").item(0);
+  const nav = document.getElementById("header");
+
+  function checkScroll() {
+    const isPastImage = window.scrollY > mainimage.clientHeight;
+    if (isPastImage){
+      nav.classList.add('scrolled');
+      console.log("ahoj");
+    }
+     
+    else nav.classList.remove('scrolled');
+  }
+  checkScroll();
+  window.addEventListener('scroll', checkScroll);
+
+  document.getElementById('navTrigger').addEventListener('click', toggleMenu);
+});
+
+function toggleMenu() {
+  const x = document.getElementById("myNav");
+  x.classList.toggle("responsive");
+}
+/*$(document).ready(function () {
   var $mainimage = $(".main-image");
     if ($(document).scrollTop() > $mainimage.height()) {
       var $nav = $("#header");
@@ -16,12 +39,8 @@ $(document).ready(function () {
       $nav.toggleClass('hovno', $(this).scrollTop() < $nav.height());
     }
   });
-});
+});*/
 
-function myFunction() {
-  var x = document.getElementById("myNav");
-  x.classList.toggle("responsive");
-}
 
 /*const navSlide = () => {
   const burger = document.querySelector('.navTrigger');
@@ -33,70 +52,3 @@ function myFunction() {
 navSlide();
 */
 
-
-
-/*$('.navTrigger').click(function () {
-  $(this).toggleClass('active');
-  console.log("Clicked menu");
-  header.classList.remove(".nav-menu");
-  $(".nav-menu").toggleClass("nav-resp");
-});*/
-
-
-  // navbar responsive
-/*if ($('.nav-menu').length) {
-  var $mobile_nav = $('.nav-menu').clone().prop({
-    class: 'mobile-nav d-lg-none d-lg-block'
-  });
-  $('body').append($mobile_nav);
-  $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
-  $('body').append('<div class="mobile-nav-overly"></div>');
-
-  $(document).on('click', '.mobile-nav-toggle', function(e) {
-    $('body').toggleClass('mobile-nav-active');
-    $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-    $('.mobile-nav-overly').toggle();
-  });
-
-  $(document).on('click', '.mobile-nav .drop-down > a', function(e) {
-    e.preventDefault();
-    $(this).next().slideToggle(300);
-    $(this).parent().toggleClass('active');
-  });
-
-  $(document).click(function(e) {
-    var container = $(".mobile-nav, .mobile-nav-toggle");
-    if (!container.is(e.target) && container.has(e.target).length === 0) {
-      if ($('body').hasClass('mobile-nav-active')) {
-        $('body').removeClass('mobile-nav-active');
-        $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
-        $('.mobile-nav-overly').fadeOut();
-      }
-    }
-  });
-} else if ($(".mobile-nav, .mobile-nav-toggle").length) {
-  $(".mobile-nav, .mobile-nav-toggle").hide();
-}
-
-// Navigation active state on scroll
-var nav_sections = $('section');
-var main_nav = $('.nav-menu, #mobile-nav');
-
-$(window).on('scroll', function() {
-  var cur_pos = $(this).scrollTop() + 200;
-
-  nav_sections.each(function() {
-    var top = $(this).offset().top,
-      bottom = top + $(this).outerHeight();
-
-    if (cur_pos >= top && cur_pos <= bottom) {
-      if (cur_pos <= bottom) {
-        main_nav.find('li').removeClass('active');
-      }
-      main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
-    }
-    if (cur_pos < 300) {
-      $(".nav-menu ul:first li:first").addClass('active');
-    }
-  });
-});*/
